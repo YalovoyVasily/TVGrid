45a3617cbb098f96d502ad447413f72d48bdddf0
Qwwety <80448769+Qwwety@users.noreply.github.com>
2023-05-23 02:06:21 +0300

    Added  Password and  UserName

---
+++ TVGrid/Migrations/20230522230240_AddedPasswordAndUserName.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TVGrid.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPasswordAndUserName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Password",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "User");
+        }
+    }
+}
