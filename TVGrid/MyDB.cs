4abd22d601b461542466f5a09aa3e9aa83eed490
Qwwety <80448769+Qwwety@users.noreply.github.com>
2023-05-22 16:14:04 +0300

    FIx BD

--- TVGrid/MyDB.cs
+++ TVGrid/MyDB.cs
@@ -54,6 +54,7 @@ namespace TVGrid
         public int Id { get; set; }
         public DateTime TimeStart { get; set; }
         public DateTime TimeEnd { get; set; }
+        public int ProgramId { get; set; }
 
         public IEnumerable<Program> Programs { get; set; }
     }
@@ -76,8 +77,6 @@ namespace TVGrid
 
         public DateTime TimeStart { get; set; }
         public DateTime TimeEnd { get; set; }
-
-        public IEnumerable<Program> Programs { get; set; }
     }
 }
 
