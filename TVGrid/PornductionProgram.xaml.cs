0d7cb86d9de23066d681076c24ca10fc3f1be417
Mater <matvey.al.al@yandex.ru>
2023-05-25 00:33:32 +0300

    Matvey (dont look po ebaly styk)

---
+++ TVGrid/PornductionProgram.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TVGrid
+{
+    /// <summary>
+    /// Логика взаимодействия для PornductionProgram.xaml
+    /// </summary>
+    public partial class PornductionProgram : Page
+    {
+        public PornductionProgram()
+        {
+            InitializeComponent();
+        }
+        private void lbl1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Label lbl = (Label)sender;
+            DragDrop.DoDragDrop(lbl, lbl.Content, DragDropEffects.Copy);
+        }
+
+        private void txtTarget_Drop(object sender, DragEventArgs e)
+        {
+            ((TextBlock)sender).Text = (string)e.Data.GetData(DataFormats.Text);
+        }
+    }
+}
