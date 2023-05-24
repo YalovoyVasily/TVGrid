1341b4a9b0171f544c76f9012d2179089174644e
Qwwety <80448769+Qwwety@users.noreply.github.com>
2023-05-25 02:26:29 +0300

    Removed the garbage

--- TVGrid/LoginForm.xaml.cs
+++ TVGrid/LoginForm.xaml.cs
@@ -46,58 +46,6 @@ namespace TVGrid
             //insert into [Role] (Title, Description) values ('Админ', 'Админ системы')
             MyDB db = new();
 
-            //var dff =new DateTime(2023,5,19,00,00,00); //2023-05-19 00:00:00.000
-            //var dff1 =new DateTime(2023,5,19,23,59,59); //2023-05-19 00:00:00.000
-
-            //await dfsf.Get(dff, dff1);
-
-            //var Schedule1 = new Schedule();
-
-            //Schedule1.TimeStart= new DateTime(2023, 5, 19, 00, 00, 00);
-            //Schedule1.TimeEnd= new DateTime(2023, 5, 19, 23, 59, 59);
-            //Schedule1.ProgramID= 2;
-            
-
-            //var Schedule2 = new Schedule();
-
-            //Schedule2.TimeStart = new DateTime(2024, 7, 19, 00, 00, 00);
-            //Schedule2.TimeEnd = new DateTime(2024, 7, 19, 23, 59, 59);
-            //Schedule2.ProgramID = 2;
-
-            //var Schedulelist= new List<Schedule>() {Schedule1, Schedule2 };
-
-            //await dfsf.Save(Schedulelist);
-
-            //var Schedule3 = new Schedule();
-
-            //Schedule3.TimeStart = new DateTime(2000, 7, 19, 00, 00, 00);
-            //Schedule3.TimeEnd = new DateTime(2000, 7, 19, 23, 59, 59);
-            //Schedule3.ProgramID = 1;
-            //Schedule3.Id = 12;
-
-            //var SchedulelistToUpdate = new List<Schedule>() { Schedule3 };
-
-            //await dfsf.Update(SchedulelistToUpdate);
-
-
-            var Schedule3 = new Schedule();
-
-            Schedule3.TimeStart = new DateTime(2000, 7, 19, 00, 00, 00);
-            Schedule3.TimeEnd = new DateTime(2000, 7, 19, 23, 59, 59);
-            Schedule3.ProgramID = 1;
-            Schedule3.Id = 12;
-
-            var Schedule4 = new Schedule();
-
-            Schedule4.TimeStart = new DateTime(2000, 7, 19, 00, 00, 00);
-            Schedule4.TimeEnd = new DateTime(2000, 7, 19, 23, 59, 59);
-            Schedule4.ProgramID = 1;
-            Schedule4.Id = 14;
-
-            var SchedulelistToDelete = new List<Schedule>() { Schedule3, Schedule4 };
-
-            await dfsf.Delete(SchedulelistToDelete);
-
 
             User user = await db.User.Include(u => u.Role).FirstOrDefaultAsync(u =>  u.UserName == tbLogin.Text && u.Password.ToString() == tbPass.Password);
             if (user == null)
